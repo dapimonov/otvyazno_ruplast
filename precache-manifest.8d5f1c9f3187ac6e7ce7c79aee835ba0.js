e7c72f27898f4e2c2cfc7a12a77ac9ab5293bd49
Dmitrii Pimonov <dapimonov@gmail.com>
2020-08-01 19:23:41 +0300

    Updates

---
+++ precache-manifest.8d5f1c9f3187ac6e7ce7c79aee835ba0.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd787300021a79d580cdf10954d8aed",
+    "url": "/otvyazno_ruplast/index.html"
+  },
+  {
+    "revision": "8d3d57c9f686b43368ab",
+    "url": "/otvyazno_ruplast/static/css/main.abe2b2df.chunk.css"
+  },
+  {
+    "revision": "ff94766897e048e6341c",
+    "url": "/otvyazno_ruplast/static/js/2.3022ad23.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/otvyazno_ruplast/static/js/2.3022ad23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d3d57c9f686b43368ab",
+    "url": "/otvyazno_ruplast/static/js/main.9f2856fd.chunk.js"
+  },
+  {
+    "revision": "bba2253f5294ed5eed1d",
+    "url": "/otvyazno_ruplast/static/js/runtime-main.05d50434.js"
+  },
+  {
+    "revision": "a6df5f781a92d59252353348b6f84af6",
+    "url": "/otvyazno_ruplast/static/media/angles.a6df5f78.jpeg"
+  },
+  {
+    "revision": "aab70e4b54735acd8e70f78426e79b9e",
+    "url": "/otvyazno_ruplast/static/media/arrow.aab70e4b.svg"
+  },
+  {
+    "revision": "5c73fac2251a0a0d7365868bb37b85cc",
+    "url": "/otvyazno_ruplast/static/media/call.5c73fac2.svg"
+  },
+  {
+    "revision": "9e810451f1806969016917566453028c",
+    "url": "/otvyazno_ruplast/static/media/concrete1.9e810451.jpeg"
+  },
+  {
+    "revision": "22e6b9d38b8710141bdf79b083cd164e",
+    "url": "/otvyazno_ruplast/static/media/concrete2.22e6b9d3.jpeg"
+  },
+  {
+    "revision": "e1229397d6887be9595fc28b18f5bc7a",
+    "url": "/otvyazno_ruplast/static/media/downloads.e1229397.svg"
+  },
+  {
+    "revision": "2d48dfcd890a888edf16f543b8b2e4a5",
+    "url": "/otvyazno_ruplast/static/media/grain.2d48dfcd.svg"
+  },
+  {
+    "revision": "291e0ec707898cc66bc9dd87fe955a0c",
+    "url": "/otvyazno_ruplast/static/media/hands.291e0ec7.svg"
+  },
+  {
+    "revision": "3bee166d2804b7b6c83922d0a05c8ae8",
+    "url": "/otvyazno_ruplast/static/media/laptop.3bee166d.svg"
+  },
+  {
+    "revision": "3fa393e8400c556c048355f44883d1a5",
+    "url": "/otvyazno_ruplast/static/media/logo.3fa393e8.svg"
+  },
+  {
+    "revision": "e467afe824df8adf2b0bba523d9cfa0d",
+    "url": "/otvyazno_ruplast/static/media/lorry.e467afe8.svg"
+  },
+  {
+    "revision": "5ac9e5bbf3c09c4ab58b433ea77a41ef",
+    "url": "/otvyazno_ruplast/static/media/nhk.5ac9e5bb.svg"
+  },
+  {
+    "revision": "cbac5dd68a04781670a7654bd329507a",
+    "url": "/otvyazno_ruplast/static/media/notebook.cbac5dd6.svg"
+  },
+  {
+    "revision": "20fbab01875234b25eab914cb37fffbf",
+    "url": "/otvyazno_ruplast/static/media/office.20fbab01.svg"
+  },
+  {
+    "revision": "f53afa252ab7b074d0457eb99f4f1774",
+    "url": "/otvyazno_ruplast/static/media/phone.f53afa25.svg"
+  },
+  {
+    "revision": "3ade7d21bca0e5fc34ee713705419016",
+    "url": "/otvyazno_ruplast/static/media/pin.3ade7d21.svg"
+  },
+  {
+    "revision": "771f6416dee94e69fe361d01ae4a47d2",
+    "url": "/otvyazno_ruplast/static/media/plane.771f6416.svg"
+  },
+  {
+    "revision": "d6a874d7866ef56e9714ba0403e63a9d",
+    "url": "/otvyazno_ruplast/static/media/question.d6a874d7.svg"
+  },
+  {
+    "revision": "acb42de1a65a226c3bb9120c529bcdfb",
+    "url": "/otvyazno_ruplast/static/media/spoon.acb42de1.svg"
+  },
+  {
+    "revision": "44697951804d20344353610244da3e72",
+    "url": "/otvyazno_ruplast/static/media/support.44697951.svg"
+  },
+  {
+    "revision": "6eb789e58d0ab5d56af1e20dd29914a4",
+    "url": "/otvyazno_ruplast/static/media/triangle.6eb789e5.svg"
+  },
+  {
+    "revision": "db4376d498635e4fef41a0a8f7ffe745",
+    "url": "/otvyazno_ruplast/static/media/truck.db4376d4.svg"
+  },
+  {
+    "revision": "118585c581ed3c487df542e248101e8e",
+    "url": "/otvyazno_ruplast/static/media/ubuntu.118585c5.woff"
+  },
+  {
+    "revision": "a85806a60cdac285443069f893db7372",
+    "url": "/otvyazno_ruplast/static/media/warehouse.a85806a6.svg"
+  }
+]);
